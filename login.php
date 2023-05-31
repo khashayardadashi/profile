31c13d8970c6eb3b4ee88009116af905a4f81024
Khashayar <115826321+khashayardadashi@users.noreply.github.com>
2023-05-31 14:33:09 +0400

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,21 @@
-
+<!doctype html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>khashayar.ir</title>
+</head>
+<body>
+<div class='main'>
+        <form action="panel.php" method="post">
+            <label>نام کاربری:</label>
+            <input type="text" name="username">
+            <label>ایمیل:</label>
+            <input type="email" name="email">
+            <label>رمز عبور:</label>
+            <input type="password" name="password">
+            <button  class='btn' type="submit" name="btn"><b>ورود</b></button>
+            <button class='btn' type="submit"><b>ثبت نام</b></button>
+        </form>
+</div>
+</body>
+</html>
